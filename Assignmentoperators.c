32030e3b539e291f627889f53c6e80d7a3f7966f
rukmi <rukmi>
2026-08-13 17:57:27 +0530

    Add assignment operators program

---
+++ Assignmentoperators.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main(){
+    int a,b;
+    a=10;
+    b=5;
+    printf("A value is=%d\n",a);
+    a+=5;
+    printf("After A value is=%d\n",a);
+    printf("Sub=%d\n",a-b);
+    a-=3;
+    printf("After Sub value is=%d\n",a);
+    a*=4;
+    printf("A value is=%d\n",a);
+    a/=2;
+    printf("A value is=%d\n",a);
+    a%=5;
+    printf("A value is=%d\n",a);
+}
