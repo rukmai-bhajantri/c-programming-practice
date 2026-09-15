78760611662d2f029c46178b9a30e18b9d22082a
Rukmai Bhajantri <rukmaibhajantri1@gmail.com>
2026-09-15 20:54:29 +0530

    Add table program

---
+++ Table.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+    int i,j,n;
+    printf("Enter value of N:\n");
+    scanf("%d",&n);
+    
+    for(i=2;i<=2;i++){
+        for(j=1;j<=n;j++){
+            printf("%d \n",i*j);
+        }
+        printf("\n");
+    }
+}
